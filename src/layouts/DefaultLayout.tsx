954953c44815c9289649fe9b62b409cf1ca90b6e
thauanyAA <thauanyaraujo5@gmail.com>
2025-04-07 22:35:31 -0300

    Init Ignite Timer app

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header";
+
+export function DefaultLayout() {
+  return (
+    <div>
+      <h1>Default Layout</h1>
+      <Header />
+      Conteudo
+    </div>
+  )
+}
